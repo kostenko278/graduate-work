8e42979164c270d1746d4fb0c0e0d730809d5907
kostenko278 <113156344+kostenko278@users.noreply.github.com>
2024-05-04 13:22:18 +0700

    обновление игры

--- game1/script.js
+++ game1/script.js
@@ -3,21 +3,29 @@ let randomWord = words[Math.floor(Math.random() * words.length)];
 let guessedWord = '_'.repeat(randomWord.length);
 
 const wordToGuessElement = document.getElementById('wordToGuess');
+const letterInput = document.getElementById('letterInput');
 wordToGuessElement.textContent = guessedWord;
 
 document.getElementById("restartButton").addEventListener("click", function() {
     window.location.reload(); // Перезагружает страницу
 });
 
+letterInput.addEventListener('keydown', function(event) {
+    if(event.key === 'Enter') {
+        checkLetter();
+    }
+});
+
 function checkLetter() {
-    const letterInput = document.getElementById('letterInput').value.toLowerCase();
-    if (letterInput.length !== 1) {
+    const letterInputValue = letterInput.value.toLowerCase();
+
+    if (letterInputValue.length !== 1) {
         alert('Пожалуйста, введите только одну букву.');
     } else {
         let newGuessedWord = '';
         for (let i = 0; i < randomWord.length; i++) {
-            if (randomWord[i] === letterInput) {
-                newGuessedWord += letterInput;
+            if (randomWord[i] === letterInputValue) {
+                newGuessedWord += letterInputValue;
             } else {
                 newGuessedWord += guessedWord[i];
             }
@@ -29,7 +37,9 @@ function checkLetter() {
             wordToGuessElement.textContent = guessedWord;
             if (guessedWord === randomWord) {
                 document.getElementById('message').textContent = 'Поздравляем, вы угадали слово!';
+                wordToGuessElement.classList.add('guessed');
             }
         }
+        letterInput.value = '';
     }
 }
